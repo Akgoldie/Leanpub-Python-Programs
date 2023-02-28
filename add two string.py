f3af0219cf6289f75dc78a003c898d82f2df172f
Arun Kumar <112628113+Akgoldie@users.noreply.github.com>
2023-02-28 15:07:30 +0530

    Add files via upload

---
+++ add two string.py
@@ -0,0 +1,8 @@
+"""a="Arun"
+b="kumar" """
+
+
+a=input("Enter the first string: ")
+b=input("Enter the second string: ")
+c=a+b
+print("Add of two string: ",c)
---
+++ addition two numbers.py
@@ -0,0 +1,6 @@
+#a=5
+#b=6
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+c=a+b
+print("Addition of two numbers: ",c)
